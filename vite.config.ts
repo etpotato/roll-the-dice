1163edce8e73961b913d63fa950bf5db776d1361
etpotato <mareev.pv@gmail.com>
2023-02-04 12:36:17 +0500

    separate vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    splitVendorChunkPlugin(),
     svelte(),
     VitePWA({
       devOptions: {
